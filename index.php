87efd1ebcfc1c8a328ec6fc47f30df5c423d594c
Rácz Igor <racz.igor@gmail.com>
2020-07-02 16:07:47 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE HTML>
+<!--
+	Full Motion by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Portfolió</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="/css/hover.css">
+	</head>
+	<body id="top">
+				<section id="banner" data-video="images/banner">
+					<div class="inner">
+						<header>
+							<h1>Rácz Igor</h1>
+							<p class="text-center">Szenvedélyes fejlesztő, aki szeretne minnél több modern technológiát, fejlesztési módszert <br> és programozási nyelvet elsajátítani.</p>
+						</header>
+						<a href="#main" class="more">Learn More</a>
+					</div>
+				</section>
+
+			<!-- Main -->
+				<div id="main">
+					<div class="inner">
+					<!-- Boxes -->
+						<div class="thumbnails">
+							<div class="box">
+								<a href="https://fetish-section.herokuapp.com/" target="_blank" class="image fit"><img class="hvr-grow" src="/images/fetis-section.PNG" alt="fetish-section.img"/></a>
+								<div class="inner">
+									<h3>Fetish Section Bandsite</h3>
+									<p>Egy szegedi zenekarnak készített weboldal.</p>
+									<a href="https://fetish-section.herokuapp.com/" target="_blank" class="button fit" data-poptrox="youtube,800x400">Demo</a>
+									<a href="https://github.com/Dextor3400/bandsitefrontonly" target="_blank" class="button fit" data-poptrox="youtube,800x400">Github</a>
+								</div>
+							</div>
+
+							<div class="box">
+								<a href="https://instaphotofront.herokuapp.com/" target="_blank" class="image fit"><img class="hvr-grow" src="images/instaphoto.png" alt="instaphoto.png" /></a>
+								<div class="inner">
+									<h3>InstaPhoto</h3>
+									<p>Instagram inspirálta fotó megosztó weboldal.</p>
+									<a href="https://instaphotofront.herokuapp.com/" target="_blank" class="button style2 fit" data-poptrox="youtube,800x400">Demo</a>
+									<a href="https://github.com/Dextor3400/instaphotofront" target="_blank" class="button style2 fit"
+										data-poptrox="youtube,800x400">Github</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div id="main">
+					<div class="inner">
+						<!-- Boxes -->
+						<div class="thumbnails">				
+							<div class="box">
+								<a href="https://facetomesite.herokuapp.com/" target="_blank" class="image fit"><img class="hvr-grow"
+										src="/images/facetome.PNG" alt="facetome.png" /></a>
+								<div class="inner">
+									<h3>FaceTome</h3>
+									<p>Egy facebook inspirálta weboldal főoldala.</p>
+									<a href="https://facetomesite.herokuapp.com/" target="_blank" class="button style3 fit"
+										data-poptrox="youtube,800x400">Demo</a>
+									<a href="https://github.com/Dextor3400/FaceTome" target="_blank" class="button style3 fit"
+										data-poptrox="youtube,800x400">Github</a>
+								</div>
+							</div>
+				
+							<div class="box">
+								<a href="https://youtu.be/s6zR2T9vn2c" class="image fit"><img src="/images/rammstein.PNG" alt="" /></a>
+								<div class="inner">
+									<h3>Rammstein Bandsite</h3>
+									<p>Egy híres zenekar weboldalának újragondolása.</p>
+									<a href="https://rammsteinfront.herokuapp.com/" target="_blank" class="button style2 fit"
+										data-poptrox="youtube,800x400">Demo</a>
+									<a href="https://github.com/Dextor3400/rammsteinsite" target="_blank" class="button style2 fit"
+										data-poptrox="youtube,800x400">Github</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			<!-- Footer -->
+				<footer id="footer">
+					<div class="inner">
+						<h2>Jelenleg pozíciót keresek webfejlesztőként.</h2>
+						<p>2020 Januárjában kezdtem el el komolyan foglalkozni a fejlesztéssel, szeretném fejleszteni a tudásomat és új technológiákat megtanulni, hogy minnél sokrétűbb tudásra tegyek szert.</p>
+
+						<ul class="icons">
+							<li><a href="https://github.com/Dextor3400" target="_blank" class="icon fa-github"><span class="label">Github</span></a></li>
+							<li><a href="https://www.linkedin.com/in/r%C3%A1cz-igor-20b9721b1/" target="_blank" class="icon fa-linkedin"><span class="label">Linkedin</span></a></li>
+							<li><a href="https://www.facebook.com/RaczIgor" target="_blank" class="icon fa-facebook"><span class="label">Facebook</span></a></li>			
+							<li><a href="mailto:racz.igor@gmail.com" class="icon fa-envelope"><span class="label">Email</span></a></li>
+						</ul>
+						<p class="copyright">&copy; Untitled. Design: <a href="https://templated.co">TEMPLATED</a>. Videos: <a href="http://coverr.co/">Coverr</a>.</p>
+					</div>
+				</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
